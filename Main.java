fde7bf1396554dcfb99fda9ab1860522d48b7d49
abhishek_a <abhishek_a@epiclanka.net>
2023-10-18 11:16:18 +0530

    Content Updated

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * Title: GDSE59-1st-Sem
+ * Description: Main Class
+ * Created by abhishek_a on 10/18/2023
+ * Email: abhishek_a@epiclanka.net
+ * Company: Epic Lanka (Pvt) Ltd.
+ * Java Version: 17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First - 1");
+        System.out.println("Second - 2");
+
+        int x = 10;
+        if(x == 10) {
+            System.out.println("If condition is called");
+        }else {
+            System.out.println("Else condition is called");
+        }
+    }
+}
