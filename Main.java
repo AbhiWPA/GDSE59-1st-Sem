a9e905b3a220ade7d2128ab99fab791858da56db
Abhishek Ashinsa <101162407+AbhiWPA@users.noreply.github.com>
2023-10-18 17:44:19 +0530

    Update Main.java
    updated

--- Main.java
+++ Main.java
@@ -18,6 +18,6 @@ public class Main {
             System.out.println("Else condition is called");
         }
 
-        System.out.println("Updated");
+        System.out.println("class updated");
     }
 }
