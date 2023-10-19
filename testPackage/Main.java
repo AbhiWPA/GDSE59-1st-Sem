c4f63334d630b9cc1e89d987df6c37edeffd741d
abhishek_a <abhishek_a@epiclanka.net>
2023-10-19 10:13:08 +0530

    content updated

---
+++ testPackage/Main.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+/**
+ * Title: GDSE59-1st-Sem
+ * Description: Main Class
+ * Created by abhishek_a on 10/19/2023
+ * Email: abhishek_a@epiclanka.net
+ * Company: Epic Lanka (Pvt) Ltd.
+ * Java Version: 17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("testPackage/Main.java is called");
+    }
+}
